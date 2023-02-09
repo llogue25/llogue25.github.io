cdecd03c7f3664d8edc2fa759fb6ca55456418b6
leologue03 <123833716+llogue25@users.noreply.github.com>
2023-02-09 18:50:29 +0000

    render pyramid to the DOM instead of console

--- mario.js
+++ mario.js
@@ -1,40 +1,35 @@
+// getting height
 
-printPyramid();
-// TODO #2
-// Take in user input for the height
+let height = parseInt(prompt("Enter a height: "))
+printPyramid(height);
 
 
-/*
- * printPyramid
- *
- * Prints to the console a pyramid of '#' characters of the specified height
- * For example, if height is 5, the console will look like this:
- *          ##
- *         ###
- *        ####
- *       #####
- *      ######
- */
+
 function printPyramid(height) {
-    console.log("Uh oh... the pyramid is under construction.");
-    console.log("Check back soon, our developers are hard at work as we speak!");
+  console.log("Uh oh... the pyramid is under construction.");
+  console.log("Check back soon, our developers are hard at work as we speak!");
+
+  // TODO #1
+  // print that pyramid!
+  
+
+  for (let row = 0; row < height; row++) {   
     
+    let bricks = row + 2;            // the amount of bricks
+    let space = height - row - 1;    // the spaces needed
 
-    // TODO #1
-    // print that pyramid!
-
-    //getting user input
-    let n = parseInt(prompt("Enter"));
-    let string = "";   //getting an empty string
-
-    for(let i =1; i <=n; i++){   // outer loop
-        for (let j = 0; j< n-i; j++){      //inner loop
-            string += " ";      //prints out spaces
-        }
-        for (let k =-1; k < i; k++){  // prints out the bricks
-            string += "#";
-        }
-        string += "\n";
+    let rowStr = "";    // empty string
+    for (let i = 0;i < space; i++){
+      let space = ".";  // the spaces between the bricks
+      rowStr += space;    
     }
-console.log(string)
+    for (let i = 0; i < bricks; i++) {    // the brick making
+      rowStr += "#"
+    }
+  console.log(rowStr)
+  //DOM
+  rowElem = document.createElement("p");
+  rowElem.innerHTML = rowStr;
+  document.getElementById("pyramid").appendChild(rowElem);
+  }
 }
