d6ab52c1ff452c55cbe912d8efb9ea2f599e3234
leologue03 <123833716+llogue25@users.noreply.github.com>
2023-02-14 04:19:49 +0000

    allow user to input pyramid heigh and respond to button click

--- mario.js
+++ mario.js
@@ -1,35 +1,47 @@
 // getting height
+//let height = parseInt(prompt("Enter a height: "))
 
-let height = parseInt(prompt("Enter a height: "))
-printPyramid(height);
-
+printPyramid();
 
 
 function printPyramid(height) {
-  console.log("Uh oh... the pyramid is under construction.");
-  console.log("Check back soon, our developers are hard at work as we speak!");
+    // clears the content 
+    document.getElementById("pyramid").innerHTML = "";
 
-  // TODO #1
-  // print that pyramid!
-  
+    //console announcment
+    console.log("Uh oh... the pyramid is under construction.");
+    console.log("Check back soon, our developers are hard at work as we speak!");
 
-  for (let row = 0; row < height; row++) {   
+    // TODO #1
+    // print that pyramid!
     
-    let bricks = row + 2;            // the amount of bricks
-    let space = height - row - 1;    // the spaces needed
+    for (let row = 0; row < height; row++) {   
+        
+        let bricks = row + 2;            // the amount of bricks
+        let space = height - row - 1;    // the spaces needed
 
-    let rowStr = "";    // empty string
-    for (let i = 0;i < space; i++){
-      let space = ".";  // the spaces between the bricks
-      rowStr += space;    
+        let rowStr = "";    // empty string
+        for (let i = 0;i < space; i++){
+        let space = ".";  // the spaces between the bricks
+        rowStr += space;    
+        }
+        for (let i = 0; i < bricks; i++) {    // the brick making
+        rowStr += "#"
+        }
+    console.log(rowStr)
+    //DOM
+    rowElem = document.createElement("p");
+    rowElem.innerHTML = rowStr;
+    document.getElementById("pyramid").appendChild(rowElem);
     }
-    for (let i = 0; i < bricks; i++) {    // the brick making
-      rowStr += "#"
     }
-  console.log(rowStr)
-  //DOM
-  rowElem = document.createElement("p");
-  rowElem.innerHTML = rowStr;
-  document.getElementById("pyramid").appendChild(rowElem);
-  }
+
+    // the input text box and button functions
+function myFunction(){
+    // gets the height 
+    let x = document.getElementById("height").value;
+    // prints the height the user enters and puts it into our pyramid function
+    document.getElementById("height").innerHTML = printPyramid(x);
+
 }
+
